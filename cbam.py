de99fb7f258e93543ce4a9c44e50b09bfb2f994b
mattroz <matveyrozanov@gmail.com>
2019-08-28 16:34:16 +0300

    Refactor modules constructors: now you need to throw in_channels only

--- cbam.py
+++ cbam.py
@@ -1,28 +1,22 @@
-import os
-
 import torch
-
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 
 
 class CAM(nn.Module):
     """Channel Attention Module
     """
 
-    def __init__(self, channels, h, w, reduction_ratio=16):
+    def __init__(self, in_channels, reduction_ratio=16):
         super().__init__()
-        self.maxpool = nn.MaxPool2d(kernel_size=(h,w), stride=(h,w))
-        self.avgpool = nn.AvgPool2d(kernel_size=(h,w), stride=(h,w))
 
-        reduced_channels_num = (channels // reduction_ratio) if (channels >= reduction_ratio) else 1
+        reduced_channels_num = (in_channels // reduction_ratio) if (in_channels >= reduction_ratio) else 1
         pointwise_in = nn.Conv2d(kernel_size=1,
-                                 in_channels=channels,
+                                 in_channels=in_channels,
                                  out_channels=reduced_channels_num)
         pointwise_out = nn.Conv2d(kernel_size=1,
                                  in_channels=reduced_channels_num,
-                                 out_channels=channels)
+                                 out_channels=in_channels)
         self.MLP = nn.Sequential(
             pointwise_in,
             nn.ReLU(),
@@ -32,8 +26,9 @@ class CAM(nn.Module):
 
 
     def forward(self, input_tensor):
-        max_feat = self.maxpool(input_tensor)
-        avg_feat = self.avgpool(input_tensor)
+        h, w = input_tensor.size(2), input_tensor.size(3)
+        max_feat = F.max_pool2d(input_tensor, kernel_size=(h, w), stride=(h, w))
+        avg_feat = F.avg_pool2d(input_tensor, kernel_size=(h, w), stride=(h, w))
         max_feat_mlp = self.MLP(max_feat)
         avg_feat_mlp = self.MLP(avg_feat)
         channel_attention_map = self.sigmoid(max_feat_mlp + avg_feat_mlp)
@@ -43,16 +38,9 @@ class CAM(nn.Module):
 class SAM(nn.Module):
     """Spatial Attention Module"""
 
-    def __init__(self, channels, h, w, ks=7):
+    def __init__(self, ks=7):
         super().__init__()
-        self.c = channels
-        self.h = h
-        self.w = w
-        self.maxpool = nn.MaxPool1d(kernel_size=channels, stride=channels)
-        self.avgpool = nn.AvgPool1d(kernel_size=channels, stride=channels)
-
-        # Handle situation if feature tensor dims are less than default kernel size (which is 7, acc. to paper)
-        self.ks = min(h,w) if ((h < 7) or (w < 7)) else ks
+        self.ks = ks
         self.sigmoid = nn.Sigmoid()
 
     def _get_padding(self,
@@ -63,29 +51,36 @@ class SAM(nn.Module):
         return padding
 
     def forward(self, input_tensor):
-        # Permute input tensor for being able to apply MaxPool and AvgPool along the channel axis
-        permuted = input_tensor.view(-1, self.c, self.h * self.w).permute(0,2,1)
-        max_feat = self.maxpool(permuted)
-        max_feat = max_feat.permute(0,2,1).view(-1, 1, self.h, self.w)
+        c, h, w = input_tensor.size(1), input_tensor.size(2), input_tensor.size(3)
 
-        avg_feat = self.avgpool(permuted)
-        avg_feat = avg_feat.permute(0,2,1).view(-1, 1, self.h, self.w)
+        # Permute input tensor for being able to apply MaxPool and AvgPool along the channel axis
+        permuted = input_tensor.view(-1, c, h * w).permute(0,2,1)
+        max_feat = F.max_pool1d(permuted, kernel_size=c, stride=c)
+        max_feat = max_feat.permute(0,2,1).view(-1, 1, h, w)
+
+        avg_feat = F.avg_pool1d(permuted, kernel_size=c, stride=c)
+        avg_feat = avg_feat.permute(0,2,1).view(-1, 1, h, w)
 
         concatenated = torch.cat([max_feat, avg_feat], dim=1)
         # Get pad values for SAME padding for conv2d
         h_pad = self._get_padding(concatenated.shape[2], self.ks, 1)
         w_pad = self._get_padding(concatenated.shape[3], self.ks, 1)
+        # Handle situation if feature tensor dims are less than default kernel size (which is 7, acc. to paper)
+        self.ks = min(h, w) if ((h < 7) or (w < 7)) else self.ks
         spatial_attention_map = self.sigmoid(
             nn.Conv2d(kernel_size=self.ks, in_channels=2, out_channels=1, padding=(h_pad, w_pad))(concatenated)
         )
         return spatial_attention_map
 
 class CBAM(nn.Module):
+    """Convolutional Block Attention Module
+    https://eccv2018.org/openaccess/content_ECCV_2018/papers/Sanghyun_Woo_Convolutional_Block_Attention_ECCV_2018_paper.pdf
+    """
 
-    def __init__(self, channels, h, w):
+    def __init__(self, in_channels):
         super().__init__()
-        self.CAM = CAM(channels, h, w)
-        self.SAM = SAM(channels, h, w)
+        self.CAM = CAM(in_channels)
+        self.SAM = SAM()
 
     def forward(self, input_tensor):
         channel_att_map = self.CAM(input_tensor)
